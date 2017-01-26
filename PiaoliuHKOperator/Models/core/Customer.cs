736333b4949d04a00820d6a9a3d22fc8a9b0f3bd
ZhiyongNi <apollo-nzy@163.com>
2017-01-26 22:27:08 +0800

    submit

--- PiaoliuHKOperator/Models/core/Customer.cs
+++ PiaoliuHKOperator/Models/core/Customer.cs
@@ -9,38 +9,21 @@ namespace PiaoliuHKOperator.Models.core
     class Customer
     {
         public int CustomerID { get; set; }
-
-        public string CustomerName;
-
-        public string CustomerPassword;
-
-        public string CustomerRealName;
-
-        public int CustomerGender;
-
-        public string CustomerSelfMobile;
-
-        public string CustomerSelfDefaultAddress;
-
-        public string CustomerSelfDirectAddress;
-
-        public string CustomerSelfOtherAddress;
-
-        public string CustomerCollage;
-
-        public string CustomerMail;
-
-        public string CustomerQQ;
-
-        public string CustomerWeixin;
-
-        public string CustomerAlipay;
-
-        public string CustomerAvatarMobile;
-
-        public string CustomerAvatarAddress;
-
-        public int CustomerAccountStatus;
-
+        public string CustomerName { get; set; }
+        public string CustomerPassword { get; set; }
+        public string CustomerRealName { get; set; }
+        public int CustomerGender { get; set; }
+        public string CustomerSelfMobile { get; set; }
+        public string CustomerSelfDefaultAddress { get; set; }
+        public string CustomerSelfDirectAddress { get; set; }
+        public string CustomerSelfOtherAddress { get; set; }
+        public string CustomerCollage { get; set; }
+        public string CustomerMail { get; set; }
+        public string CustomerQQ { get; set; }
+        public string CustomerWeixin { get; set; }
+        public string CustomerAlipay { get; set; }
+        public string CustomerAvatarMobile { get; set; }
+        public string CustomerAvatarAddress { get; set; }
+        public int CustomerAccountStatus { get; set; }
     }
 }
