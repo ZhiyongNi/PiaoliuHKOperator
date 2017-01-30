383e5ca8caabdd072e20c5b3bdcbbeeea659d63e
Poseidon <Poseidon@DESKTOP-Q7CJO2L>
2017-01-30 16:58:33 +0800

    submit

--- PiaoliuHKOperator/Views/TransitBillCheckoutPage.xaml.cs
+++ PiaoliuHKOperator/Views/TransitBillCheckoutPage.xaml.cs
@@ -14,6 +14,8 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
 
 // “空白页”项模板在 http://go.microsoft.com/fwlink/?LinkId=234238 上有介绍
 
@@ -41,7 +43,7 @@ namespace PiaoliuHKOperator.Views
         {
             TransitBill_Instance = this.TransitBillList_Instance.TransitBillItemList[TransitBillSelecting_ListView.SelectedIndex];
 
-            List<string> FilterArray=new List<string>();
+            List<string> FilterArray = new List<string>();
             FilterArray.Add("PackageRelatedTransitBillSerialID = \'" + TransitBill_Instance.TransitBillSerialID + "\'");
             PackageList_Instance.findAllPackagebyFilter(FilterArray);
 
@@ -78,6 +80,42 @@ namespace PiaoliuHKOperator.Views
                 FilterArray.Add("TransitBillSerialID = \'" + TransitBillSerialID_TextBox.Text + "\'");
             }*/
             return FilterArray;
+        }
+
+        private async void PrintQR_Button_Click(object sender, RoutedEventArgs e)
+        {
+            /*if (Windows.Graphics.Printing.PrintManager.IsSupported())
+            {
+                try
+                {
+                    // Show print UI
+                    await Windows.Graphics.Printing.PrintManager.ShowPrintUIAsync();
+
+                }
+                catch
+                {
+                    // Printing cannot proceed at this time
+                    ContentDialog noPrintingDialog = new ContentDialog()
+                    {
+                        Title = "Printing error",
+                        Content = "\nSorry, printing can' t proceed at this time.",
+                        PrimaryButtonText = "OK"
+                    };
+                    await noPrintingDialog.ShowAsync();
+                }
+            }
+            else
+            {
+                // Printing is not supported on this device
+                ContentDialog noPrintingDialog = new ContentDialog()
+                {
+                    Title = "Printing not supported",
+                    Content = "\nSorry, printing is not supported on this device.",
+                    PrimaryButtonText = "OK"
+                };
+                await noPrintingDialog.ShowAsync();
+            }*/
+
         }
     }
 }
